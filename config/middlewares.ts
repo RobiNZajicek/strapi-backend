0577bc6c2c04d3c956f3f73fa41c924eb7c1eb8e
RobiNZajicek <zajicekrobin3@gmail.com>
2025-04-06 20:35:06 +0200

    hpe so Obrazky-wtf-snad-snad

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,26 +1,36 @@
 module.exports = [
+  // Povinné middlewares (musí být na začátku)
+  'strapi::errors',
   {
     name: 'strapi::security',
     config: {
-      origin: [
-        'https://praguestrikingacademy.cz',
-        'https://strapi-backend-app-8zq2.onrender.com'
-      ],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
           'img-src': [
-            "'self'", 
-            'data:', 
-            'blob:', 
-            'https:', 
+            "'self'",
+            'data:',
+            'blob:',
             'https://strapi-backend-app-8zq2.onrender.com',
             'https://render.com'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://strapi-backend-app-8zq2.onrender.com'
           ],
         },
       },
     },
-  }
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public', // Tento musí být vždy poslední
 ];
