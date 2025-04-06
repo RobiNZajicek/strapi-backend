cad41a253d99fd9fd0904b088d018cf97142d775
RobiNZajicek <zajicekrobin3@gmail.com>
2025-04-06 14:58:07 +0200

    hope it all works out agian

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,29 @@
-export default [
-  'strapi::logger',
+// export default [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://praguestrikingacademy.com'], // tvoje doména
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: '*',
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
