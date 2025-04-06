18a7b0cddf63707f98b83128ce18ffb74f76f7bc
RobiNZajicek <zajicekrobin3@gmail.com>
2025-04-06 21:58:39 +0200

    cldy-os

--- config/cors.ts
+++ config/cors.ts
@@ -2,7 +2,6 @@ module.exports = {
     origin: [
       'https://praguestrikingacademy.cz',
       'https://strapi-backend-app-8zq2.onrender.com',
-      'http://localhost:1337' // pro vývoj
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
     headers: [
